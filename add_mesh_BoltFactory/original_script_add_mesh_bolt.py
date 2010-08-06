f8530c795de8e1940d0de4e99c13433d5d4afc1f
campbellbarton <campbellbarton@96b784f8-4ae3-dd11-aff7-0030488c597c>
2010-08-06 03:47:42 +0000

    update for changes in blender
    git-svn-id: https://svn.blender.org/svnroot/bf-extensions/trunk/py/scripts/addons@896 96b784f8-4ae3-dd11-aff7-0030488c597c

--- add_mesh_BoltFactory/original_script_add_mesh_bolt.py
+++ add_mesh_BoltFactory/original_script_add_mesh_bolt.py
@@ -1,25 +1,4 @@
-﻿# add_mesh_bolt.py Copyright (C) 2010, Aaron Keith (Spudmn)
-#
-# add bolt or Nut mesh blender 2.50 Found in the TOOLS panel 
-# ***** BEGIN GPL LICENSE BLOCK *****
-#
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
-# ***** END GPL LICENCE BLOCK ***** 
-
+﻿
 bl_addon_info = {
     'name': 'Add Mesh: Bolt',
     'author': 'Aaron Keith',
